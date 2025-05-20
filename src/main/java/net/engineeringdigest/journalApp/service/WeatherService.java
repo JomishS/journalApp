61daecf76b0a43c51da78ff7e44ee53a01a09e6b
Jomish <jomishshajahan@gmail.com>
2025-05-20 20:14:10 +0530

    Added redis configuration

--- src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -2,6 +2,7 @@ package net.engineeringdigest.journalApp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -23,11 +24,24 @@ public class WeatherService {
     @Autowired
     private AppCache appCache;
 
+    @Autowired
+    private RedisService redisService;
+
     public WeatherResponse getWeather(String city){
-        String finalAPI=appCache.appCache.get(AppCache.keys.WEATHER_API.toString()).replace(Placeholders.CITY, city).replace(Placeholders.API_KEY, apiKey);
-        ResponseEntity<WeatherResponse> response= restTemplate.exchange(finalAPI, HttpMethod.GET,null,WeatherResponse.class);
-        WeatherResponse body= response.getBody();
-        return body;
+        WeatherResponse weatherResponse= redisService.get("weather_of_" + city, WeatherResponse.class);
+        if(weatherResponse!=null){
+            return weatherResponse;
+        } else{
+            String finalAPI=appCache.appCache.get(AppCache.keys.WEATHER_API.toString()).replace(Placeholders.CITY, city).replace(Placeholders.API_KEY, apiKey);
+            ResponseEntity<WeatherResponse> response= restTemplate.exchange(finalAPI, HttpMethod.GET,null,WeatherResponse.class);
+            WeatherResponse body= response.getBody();
+            if(body!=null){
+                redisService.set("weather_of_" + city, body,300l);
+            }
+            return body;
+        }
+        
+        
     }
     
 }
